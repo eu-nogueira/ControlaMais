925e755545d5bd0137dcd6d703710c2d955be85a
eu-nogueira <marcosnogueirapro@gmail.com>
2026-04-23 00:13:16 -0300

    Adicionando movimentação no saldo da carteira

--- src/store/extrato.js
+++ src/store/extrato.js
@@ -2,10 +2,21 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const extrato = createSlice({
     name: 'extrato',
-    initialState: [],
+    initialState: {
+        contasExtrato: [],
+        saldoTotal: 0,
+        despesaMes: 0
+    },
     reducers: {
         adicionarConta(state, action) {
-            state.push(action.payload)
+            state.contasExtrato.push(action.payload)
+            console.log(action.payload)
+            if(action.payload.tipoConta === 'Receita') {
+                state.saldoTotal += Number(action.payload.valor)
+            } else {
+                state.saldoTotal -= Number(action.payload.valor)
+                state.despesaMes -= Number(action.payload.valor)
+            }
         },
         removerConta(state, payload) {
 
