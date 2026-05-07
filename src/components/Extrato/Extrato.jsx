b32e654b9c2db3ba6ebe497ba0e920678bb9d9d3
eu-nogueira <marcosnogueirapro@gmail.com>
2026-05-07 01:44:53 -0300

    Adiciona modal ver mais

--- src/components/Extrato/Extrato.jsx
+++ src/components/Extrato/Extrato.jsx
@@ -1,33 +1,43 @@
 import { useDispatch, useSelector } from 'react-redux'
 import './Extrato.css'
 import { MdDelete } from 'react-icons/md'
+import { CiCircleMore } from "react-icons/ci";
 import { removerConta } from '../../store/extrato'
+import { useState } from 'react';
+import Modal from '../Modal/Modal';
 
 function Extrato() {
   const contas = useSelector((state) => state.extrato.contasExtrato) || []
+  const [modal, setModal] = useState(false)
   const dispatch = useDispatch()
 
   function handleDelete(conta) {
     dispatch(removerConta(conta))
   }
 
+  function verMais() {
+    setModal(!modal)
+  }
+
   return (
+    <>
+    { modal && < Modal handleModal={setModal} />}
+
     <div className='extrato'>
         <h1>Extrato</h1>
           {contas?.length > 0 ? (
           <table>
               <thead>
                 <tr>
-                  <th>Tipo</th>
-                  <th>Data</th>
+                  <th>Data cadastro</th>
                   <th>Descrição</th>
                   <th>Valor</th>
+                  <th onClick={() => verMais()} style={{cursor: 'pointer'}}><CiCircleMore size={20} /></th>
                 </tr>
               </thead>
               <tbody>
               {contas.map((conta, index) => (
                       <tr key={index} className={conta.tipoConta === 'Despesa' ? 'despesa' : 'receita'}>
-                        <td>{conta.tipoConta}</td>
                         <td>{conta.dataCadastro}</td>
                         <td>{conta.descricao}</td>
                         <td>R${conta.valorFormatado.toFixed(2).replace('.', ',')}</td>
@@ -46,6 +56,7 @@ function Extrato() {
               </table>
             )}
     </div>
+  </>
   )
 }
 
