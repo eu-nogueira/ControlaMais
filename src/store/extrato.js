829da57d3cfcefe0f4ac5444724ca531c3649823
eu-nogueira <marcosnogueirapro@gmail.com>
2026-05-11 23:54:18 -0300

    Adiciona botão adicionar as minhas depesas

--- src/store/extrato.js
+++ src/store/extrato.js
@@ -15,7 +15,7 @@ const extrato = createSlice({
             state.contasExtrato.push(action.payload)
             if(action.payload.tipoConta === 'Receita') {
                 state.saldoTotal += Number(action.payload.valorFormatado)
-            } else {
+            } else if (action.payload.tipoConta === 'Despesa' && action.payload.selected){
                 state.saldoTotal -= Number(action.payload.valorFormatado)
                 state.despesaMes -= Number(action.payload.valorFormatado)
             }
@@ -29,7 +29,7 @@ const extrato = createSlice({
             state.contasExtrato = remove
             if(action.payload.tipoConta === 'Receita') {
                 state.saldoTotal -= action.payload.valorFormatado
-            } else {
+            } else if (action.payload.tipoConta === 'Despesa' && action.payload.selected) {
                 state.saldoTotal += action.payload.valorFormatado
                 state.despesaMes += action.payload.valorFormatado
             }
